deb2ee3fe0a2bf8b2b6eccf25fe0c3c14f95935b
0xMSherby <pr.sherby@gmail.com>
2020-03-10 14:03:16 +0200

    Update install-docker-repository.sh

--- docker-scripts/install-docker-repository.sh
+++ docker-scripts/install-docker-repository.sh
@@ -2,17 +2,17 @@
 echo "======================= Docker Remover ========================"
 echo "=============== Author 0xMSherby( TH3Captin ) ================="
 echo ""
-echo "1.Update the apt package index"
+echo "[+] 1.Update the apt package index"
 sudo apt-get update
-echo "2.Install packages to allow apt to use a repository over HTTPS"
+echo "[+] 2.Install packages to allow apt to use a repository over HTTPS"
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-echo "3.Add Docker’s official GPG key:"
+echo "[+] 3.Add Docker’s official GPG key:"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "3.Verify that you now have the key with the fingerprint "
+echo "[+] 4.Verify that you now have the key with the fingerprint "
 sudo apt-key fingerprint 0EBFCD88
 echo "======================== [Done !] ============================="
