185127771fa8e50c0b182351bfacbb7da6757172
0xMSherby <pr.sherby@gmail.com>
2020-03-10 14:01:11 +0200

    Create install-docker-repository.sh

---
+++ docker-scripts/install-docker-repository.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "======================= Docker Remover ========================"
+echo "=============== Author 0xMSherby( TH3Captin ) ================="
+echo ""
+echo "1.Update the apt package index"
+sudo apt-get update
+echo "2.Install packages to allow apt to use a repository over HTTPS"
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+echo "3.Add Docker’s official GPG key:"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "3.Verify that you now have the key with the fingerprint "
+sudo apt-key fingerprint 0EBFCD88
+echo "======================== [Done !] ============================="
